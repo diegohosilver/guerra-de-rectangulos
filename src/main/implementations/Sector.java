df3e16833f25d19956b3a1951cc261eefaf1d2e1
Diego Silvera de Souza <ctk.591@gmail.com>
2019-05-09 18:42:21 -0300

    Se agrega clase sector con atributos y getters

---
+++ src/main/implementations/Sector.java
@@ -0,0 +1,28 @@
+package main.implementations;
+
+import main.shared.Position;
+
+public class Sector {
+	private int fila;
+	private int columna;
+	private Position posicion;
+	
+	public Sector(int fila, int columna, Position posicion) {
+		this.fila = fila;
+		this.columna = columna;
+		this.posicion = posicion;
+	}
+	
+	public int getFila() {
+		return fila;
+	}
+	
+	public int getColumna() {
+		return columna;
+	}
+	
+	public Position getPosicion() {
+		return posicion;
+	}
+
+}
